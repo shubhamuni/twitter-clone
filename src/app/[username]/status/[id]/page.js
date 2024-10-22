7644984277ad20b95a29b84306c10733b3112a89
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-10-22 16:06:53 +0530

    seperating 2 post sizes

--- src/app/[username]/status/[id]/page.js
+++ src/app/[username]/status/[id]/page.js
@@ -1,6 +1,8 @@
 "use client"; // This marks the component as a client component
 
+import Layout from "@/app/component/Layout";
 import PostContent from "@/app/component/PostContent";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { use } from 'react'; // Import `use` hook
 
@@ -26,10 +28,21 @@ export default function Page({ params }) {
     }, [id]); // Add id as a dependency
 
     return (
-        <div>
+        <Layout>
             {post && (
-                <PostContent {...post} />
+                <div className="px-5 py-2">
+                    
+                    <Link href={'/'}>
+                        <div className="flex mb-5 cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-3">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                            </svg>
+                            Tweet
+                        </div>
+                    </Link>
+                    <PostContent {...post} big={true} />
+                </div>
             )}
-        </div>
+        </Layout>
     );
 }
--- src/app/component/PostContent.js
+++ src/app/component/PostContent.js
@@ -3,28 +3,45 @@ import Avatar from "./Avatar"; // Ensure this component is set up correctly
 import ReactTimeAgo from 'react-time-ago';
 import Link from 'next/link';
 
-export default function PostContent({ text, author, createdAt, _id }) {
-    console.log(author);
+export default function PostContent({ text, author, createdAt, _id, big=false }) {
+    const createdAtdate = new Date(createdAt)
 
     return (
-        <div className="text-twitterWhite flex">
-            <div>
-                <Avatar src={author.image} />
-            </div>
-            <div className="pl-2">
+        <div>
+            <div className="text-twitterWhite flex">
                 <div>
-                    <span className='font-bold'>{author.name}</span>
-                    <span className="pl-1 text-twitterLightGray">@{author.username}</span>
-                    {createdAt && (
-                        <span className="pl-1 text-twitterLightGray">
-                            <ReactTimeAgo date={createdAt} timeStyle="twitter" locale="en-US" />
-                        </span>
+                    <Avatar src={author.image} />
+                </div>
+                <div className="pl-2">
+                    <div>
+                        <span className='font-bold pr-1'>{author.name}</span>
+                        {big && <br/>}
+                        <span className="text-twitterLightGray">@{author.username}</span>
+                        {createdAt && !big && (
+                            <span className="pl-1 text-twitterLightGray">
+                                <ReactTimeAgo date={createdAt} timeStyle="twitter" locale="en-US" />
+                            </span>
+                        )}
+                    </div>
+                    {!big && (
+                    <div>
+                        <Link href={`/${author.username}/status/${_id}`}>
+                        {text}
+                        </Link>
+                    </div>
                     )}
                 </div>
-                <Link href={`/${author.username}/status/${_id}`}>
-                    {text}
-                </Link>
             </div>
+            {big && (
+            <div className='mt-2'>
+                <Link href={`/${author.username}/status/${_id}`}>
+                {text}
+                </Link>
+                    <div className='text-twitterLightGray text-sm'>
+                        {createdAtdate.toISOString().replace('T',' ').slice(0,16).split(' ').reverse().join(' ')}
+                </div>    
+            </div>
+            )}
         </div>
     );
 }
