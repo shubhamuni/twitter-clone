53fd04d760e019015214068a35850c33d0b966e5
Shubham R Patil <89505897+shubhamuni@users.noreply.github.com>
2024-10-23 21:19:35 +0530

    pushing to main

--- src/app/component/PostForm.js
+++ src/app/component/PostForm.js
@@ -63,9 +63,9 @@ export default function PostForm({ onPost }) {
             <div className="text-right mt-2 pb-2">
                 <button 
                     type="submit" 
-                    className="bg-twitterBlue text-white rounded-full px-4 py-2 font-bold hover:bg-twitterDarkBlue"
+                    className=" bg-twitterBlue text-white rounded-full px-4 py-2 font-bold hover:bg-twitterDarkBlue"
                 >
-                    Tweet
+                    Post
                 </button>
             </div>
         </form>  
